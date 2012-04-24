4afebef3058e063234ba41e8245c877d2bafa1f3
Naveen Akkapeddi <naveen.csci@gmail.com>
2012-04-24 15:13:20 -0400

    Updated README and CHANGELOG. Sorted training data into input and target lists. Created network structure with two input neurons, two hidden neurons, and one output neuron. The hidden layer uses tanh, while the output neuron uses softmax activation function.

--- aifinalproject.py
+++ aifinalproject.py
@@ -2,12 +2,14 @@ import sys
 import datadumper
 import pickle
 import math
-
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.structure import TanhLayer, SoftmaxLayer
+from pybrain.datasets import SupervisedDataSet
 
 print """Generating dataset (historical stock data)..."""
 datadumper.data_dump()
 
-myList = []
+myList = [] 	# Create empty list to store nested list containing closing price and volume
 stockFile = open('historicaldata.pickle', 'r')
 while 1:
     try:
@@ -16,10 +18,20 @@ while 1:
         break
 stockFile.close()
 
-for index1, row in enumerate(myList):
+for index1, row in enumerate(myList): # For each item in every row of the nested list, convert string to float and apply natural log function
 	for index2, item in enumerate(row):
 		try:
 			myList[index1][index2] = (math.log(float(item)))
 		except ValueError:
 			pass
-print myList
+#print myList
+
+myClonedList = myList # Alias the previous nested list, take the first item (natural log closing price) of every row and append it to new list.
+targetData = []
+for index1, row in enumerate(myClonedList):
+	targetData.append(myClonedList[index1][0])
+
+#print targetData
+
+myNet = buildNetwork(2, 2, 1, hiddenclass=TanhLayer, outclass=SoftmaxLayer) # Build network with 2 input neurons, 2 hidden neurons, and 1 output neuron
+print myNet['in'], myNet['hidden0'], myNet['out']
