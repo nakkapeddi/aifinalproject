9c185e9846a81815f39acfa1e41415b0434f542c
Naveen Akkapeddi <naveen.csci@gmail.com>
2012-04-27 18:01:33 -0400

    Success! Finally fixed issue with adding data points into the data set. TODO: Come up with a way to train the net

--- aifinalproject.py
+++ aifinalproject.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python
+#
+#  Copyright (c) 2012, Naveen Akkapeddi (naveen.csci@gmail.com)
+#
+#  license: GNU LGPL
+#
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2.1 of the License, or (at your option) any later version.
+#  
+
 import sys
 import datadumper
 import pickle
@@ -5,6 +17,7 @@ import math
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.structure import TanhLayer, SoftmaxLayer
 from pybrain.datasets import SupervisedDataSet
+from pybrain.supervised.trainers import BackpropTrainer
 
 print """Generating dataset (historical stock data)..."""
 datadumper.data_dump()
@@ -32,15 +45,21 @@ for index1, row in enumerate(myClonedList):
 	targetData.append(myClonedList[index1][0])
 
 targetData.pop() #Pop off last element because it should be the next day's closing price.
-print targetData
+#print targetData
 
 myNet = buildNetwork(2, 2, 1, hiddenclass=TanhLayer, outclass=SoftmaxLayer) # Build network with 2 input neurons, 2 hidden neurons, and 1 output neuron
 print myNet['in'], myNet['hidden0'], myNet['out']
 
 myDS = SupervisedDataSet(2, 1) # Create dataset with two dimensional input, one dimensional target.
 
-for i in range(len(myList)):
-	tupleList = tuple(myList[i])
-	tupleTarget = tuple(targetData)
-	print tupleList, tupleTarget
-#	myDS.addSample(tupleList, (0,))
+for i in enumerate(myList): #Pop off each element, then added to dataset
+	myDataSetAdder = myDS.appendLinked(myList.pop(), targetData.pop())
+
+for inpt, target in myDS: #display dataset structure
+	print inpt, target
+
+myTrainer = BackpropTrainer(myNet, myDS)
+print """This may take awhile..."""
+myTrainData, myTestData = myDS.splitWithProportion(0.25)
+print len(myTrainData), len(myTestData)
+print myTrainer.train() #TODO, setup network structure differently.
